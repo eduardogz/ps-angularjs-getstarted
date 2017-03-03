d50c408c8e7af2ea65d599ea9b1a5c7da7cdb14f
Eduardo Cruz <eduardo.cruz@ubisoft.com>
2017-03-03 09:17:28 -0500

    starting directives

---
+++ src/script/controllers.js
@@ -0,0 +1,21 @@
+(function () {
+
+    var app = angular.module("githubViewer", []);
+
+    var MainController = function ($scope, $http) {
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        };
+        var onError = function (reason) {
+            $scope.error = "Could not fetch data";
+        };
+
+        $http.get("https://api.github.com/users/eduardogz")
+            .then(onUserComplete, onError);
+
+        $scope.message = "Hey Ubi!";
+    }
+
+    app.controller("MainController", ["$scope", "$http", MainController]);
+
+})();
