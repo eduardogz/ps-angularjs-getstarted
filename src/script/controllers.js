4fbfcd556c173bf6b3346f2f97ebb50316d995b5
Eduardo Cruz <eduardo.cruz@ubisoft.com>
2017-03-06 13:22:31 -0500

    RUsing eslint defaults, not eslint standards

--- src/script/controllers.js
+++ src/script/controllers.js
@@ -1,19 +1,20 @@
 (function() {
 
-    var app = angular.module('githubViewer', []);
+  var app = angular.module('githubViewer', []);
 
-    var MainController = function($scope, $http) {
-        var onUserComplete = function(response) {
-            $scope.user = response.data;
-        };
-        var onError = function(reason) {
-            $scope.error = 'Could not fetch data';
-        };
-        $http.get('https://api.github.com/users/eduardogz')
-            .then(onUserComplete, onError);
+  var MainController = function($scope, $http) {
 
-        $scope.message = 'Hey Ubi!';
-    }
-    app.controller('MainController', ['$scope', '$http', MainController]);
+    var onUserComplete = function(response) {
+      $scope.user = response.data;
+    };
+    var onError = function(reason) {
+      $scope.error = 'Could not fetch data:' + reason;
+    };
+    $http.get('https://api.github.com/users/eduardogz')
+      .then(onUserComplete, onError);
+
+    $scope.message = 'Hey Ubi!';
+  }
+  app.controller('MainController', ['$scope', '$http', MainController]);
 
 })();
--- src/script/directives.js
+++ src/script/directives.js
@@ -1,19 +1,30 @@
-(function () {
+(function() {
 
     var app = angular.module("githubViewer", []);
 
-    var MainController = function ($scope, $http) {
-        var onUserComplete = function (response) {
+    var MainController = function($scope, $http) {
+
+        var onUserComplete = function(response) {
             $scope.user = response.data;
-        };
-        var onError = function (reason) {
-            $scope.error = "Could not fetch data";
+            $http.get($scope.user.repos_url).then(onRepos, onError);
         };
 
-        $http.get("https://api.github.com/users/eduardogz")
-            .then(onUserComplete, onError);
+        var onRepos = function(response) {
+            $scope.repos = response.data;
+        };
 
-        $scope.message = "Hey Ubi!";
+        var onError = function(reason) {
+            $scope.error = "Could not fetch data: " + reason;
+        };
+
+        $scope.search = function(username) {
+            $http.get("https://api.github.com/users/" + username)
+                .then(onUserComplete, onError);
+
+            $scope.message = "Github Viewer";
+            $scope.username = "angular";
+            $scope.repoSortOrder = "-stargazers_count";
+        }
     }
 
     app.controller("MainController", ["$scope", "$http", MainController]);
